5e1208b8f12f31d6cc31bfd84c1e575a0bc652e0
shuhongfan <shuhongfan@foxmail.com>
2022-04-06 16:03:03 +0800

    init

---
+++ sy1-2/sy1-2/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace sy1_2
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("获取www.cctv.com的所有IP地址：");
+            try
+            {
+                IPAddress[] ipAddresses = Dns.GetHostAddresses("www.cctv.com");
+                foreach (IPAddress ip in ipAddresses)
+                {
+                    sb.AppendLine(ip.ToString());
+                }
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message, "获取失败");
+            }
+
+            string hostName = Dns.GetHostName();
+            sb.AppendLine("获取本机所有IP地址:");
+            IPHostEntry me = Dns.GetHostEntry(hostName);
+            foreach (IPAddress ip in me.AddressList)
+            {
+                if (ip.AddressFamily==AddressFamily.InterNetwork)
+                {
+                    sb.AppendLine("IPV4:"+ip.ToString());
+                }
+                else if (ip.AddressFamily == AddressFamily.InterNetworkV6)
+                {
+                    sb.AppendLine("ipv6:" + ip.ToString());
+                }
+                else
+                {
+                    sb.AppendLine("其他：" + ip.ToString());
+                }
+            }
+
+            IPAddress localip = IPAddress.Parse("::1");
+            Output(sb, localip);
+            IPAddress localip1 = IPAddress.Parse("127.0.0.1");
+            Output(sb, localip1);
+
+            textBlock.Text = sb.ToString();
+        }
+
+        private static void Output(StringBuilder sb, IPAddress localip)
+        {
+            IPEndPoint iep = new IPEndPoint(localip, 80);
+            if (localip.AddressFamily == AddressFamily.InterNetworkV6)
+            {
+                sb.Append("IPv6端点： " + iep.ToString());
+            }
+            else
+            {
+                sb.Append("IPv4端点： " + iep.ToString());
+            }
+            sb.Append("，端口 " + iep.Port);
+            sb.Append("，地址 " + iep.Address);
+            sb.AppendLine("，地址族 " + iep.AddressFamily);
+        }
+
+    }
+}
---
+++ sy1-3/sy1-3/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace sy1_3
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            NetworkInterface[] adapters = NetworkInterface.GetAllNetworkInterfaces();
+            sb.AppendLine("适配器个数：" + adapters.Length);
+            int index = 0;
+            foreach (NetworkInterface adapter in adapters)
+            {
+                index++;
+                sb.AppendLine("------------第" + index + "个适配器信息" + "----------");
+                sb.AppendLine("名称：" + adapter.Name);
+                sb.AppendLine("类型：" + adapter.NetworkInterfaceType);
+                sb.AppendLine("速度：" + adapter.Speed / 1000 / 1000 + "M");
+
+                byte[] macBytes = adapter.GetPhysicalAddress().GetAddressBytes();
+                sb.AppendLine("MAC地址：" + BitConverter.ToString(macBytes));
+
+                IPInterfaceProperties adapterProperties = adapter.GetIPProperties();
+                IPAddressCollection dnsServers = adapterProperties.DnsAddresses;
+
+                if (dnsServers.Count > 0)
+                {
+                    foreach (IPAddress dns in dnsServers)
+                    {
+                        sb.AppendLine("DNs服务器Ip地址：" + dns);
+                    }
+                }
+                textBlock.Text=sb.ToString();
+            }
+        }
+    }
+}
